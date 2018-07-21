2d61812b76589718f20cd6fb0658bd8303a89e73
Ini <initex@inbox.lv>
2018-07-21 21:18:42 +0300

    homeWork1

---
+++ src/homeWork1/GenerateRandomAndCount.java
@@ -0,0 +1,19 @@
+package homeWork1;
+
+import java.util.Random;
+
+public class GenerateRandomAndCount {
+
+    public static void main(String[] args) {
+        System.out.println("Hais! Es tagad izdomāšu trīs ciparus un tos saskaitīšu");
+        Random random = new Random();
+        int numberOne = random.nextInt(100) + 0;
+        System.out.println("pirmais cipars ir " + numberOne);
+        int numberTwo = random.nextInt(100) + 0;
+        System.out.println("pirmais cipars ir " + numberTwo);
+        int numberThree = random.nextInt(100) + 0;
+        System.out.println("pirmais cipars ir " + numberThree);
+        System.out.println("Šito skaitļu summa ir "+(numberOne+numberTwo+numberThree));
+    }
+}
+
