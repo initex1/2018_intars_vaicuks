f53c5f9630e7e767f4df607ec1373b2e00ec086c
Ini <initex@inbox.lv>
2018-07-24 23:08:48 +0300

    homeWork5

---
+++ src/homeWork5/bubbleSorting.java
@@ -0,0 +1,55 @@
+package homeWork5;
+
+import java.util.Random;
+
+public class bubbleSorting {
+    public static void main(String[] args) {
+        int[] list = createAndFillList();
+
+        printList(list);
+
+        sortList(list);
+
+        System.out.println("pēc pārtošanas");
+        printList(list);
+    }
+
+    private static void sortList(int[] list) {
+        int count = 0;
+        do {
+            sortingStep(list);
+            count++;
+        } while (count != list.length);
+    }
+
+    private static void sortingStep(int[] list) {
+        for (int i = 0; i < list.length; i++) {
+            if (i != list.length - 1) {
+                if (list[i] > list[i + 1]) {
+                    int tmpValue = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = tmpValue;
+
+                }
+            }
+        }
+    }
+
+
+    private static int[] createAndFillList() {
+        int[] list = new int[10];
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            list[i] = random.nextInt(100);
+        }
+        return list;
+    }
+
+    private static void printList(int[] list) {
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i]);
+        }
+    }
+
+}
