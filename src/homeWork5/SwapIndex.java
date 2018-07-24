9d5ba8c0e6dfd6a22b2a2832a958257a9d491182
Ini <initex@inbox.lv>
2018-07-24 19:44:27 +0300

    homeWork5

--- src/homeWork5/SwapIndex.java
+++ src/homeWork5/SwapIndex.java
@@ -1,5 +1,6 @@
 package homeWork5;
 
+import java.util.Collection;
 import java.util.Random;
 
 public class SwapIndex {
@@ -17,11 +18,28 @@ public class SwapIndex {
         }
 
         int biggestInt = 0;
+        int indexOfBiggestInt = 0;
         for (int i = 0; i < list.length; i++) {
             if (biggestInt < list[i]) {
 //                System.out.println("Cipars " + list[i] + " ir lielāks par " + biggestInt);
                 biggestInt = list[i];
+                indexOfBiggestInt = i;
             }
         }
+        System.out.println("Lielākais cipars ir "+biggestInt);
+
+        if (indexOfBiggestInt != 0) {
+            int tmpValue=list[0];
+            list[0] = biggestInt;
+            list[indexOfBiggestInt] = tmpValue;
+            System.out.println("Pārvietpju \"O\" indeksa vērtību " + tmpValue + " uz indeksu " +"\""+ indexOfBiggestInt+"\"");
+        }else{
+            System.out.println("Lielākais cipars jau ir \"0\" indeksā");
+        }
+//pārbaudei
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i]);
+        }
+
     }
 }
