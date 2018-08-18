a4508bd260022b11ccd6382d2e0d23ed13c29744
Ini <initex@inbox.lv>
2018-08-18 16:37:40 +0300

    homeWork5

--- src/homeWork7/ConvertToListAndSum.java
+++ src/homeWork7/ConvertToListAndSum.java
@@ -1,7 +1,36 @@
 package homeWork7;
 
+import java.util.Scanner;
+
 public class ConvertToListAndSum {
     public static void main(String[] args) {
+        System.out.println("Iedod man listi ar cipariem, kas atdalīti ar tukšuma taustiņu");
+        String list = getString();
+        int sumOfList= getSumOfList(convertToIntList(list));
+        System.out.println("Listes ciparu summa: "+sumOfList);
+    }
 
+    private static int getSumOfList(int[] list) {
+        int sum=0;
+        for (int i=0;i<list.length;i++){
+            sum=sum+list[i];
+        }
+        return sum;
+    }
+
+    private static int[] convertToIntList(String list) {
+        String[] stringList = list.split(" ");
+        System.out.println(stringList.length);
+        int[] intList = new int[stringList.length];
+        for (int i = 0; i < stringList.length; i++) {
+            intList[i] = Integer.parseInt(stringList[i]);
+        }
+        return intList;
+    }
+
+    private static String getString() {
+        Scanner scanner = new Scanner(System.in);
+        String list = scanner.nextLine();
+        return list;
     }
 }
